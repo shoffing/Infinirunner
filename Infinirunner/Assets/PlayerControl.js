80dfa9f1d30676bfe52aa2a87f443e2358fd112a
Process39 <aida11@comcast.net>
2014-03-21 01:41:11 -0400

    Shit Note of Stuff

--- Infinirunner/Assets/PlayerControl.js
+++ Infinirunner/Assets/PlayerControl.js
@@ -1,13 +1,31 @@
 ﻿#pragma strict
 
-private final static var SPEED = 50;
+private final static var SPEED = 20;
 private final static var GRAVITY = 14;
 
+var Clap : AudioClip;
+
 private var gravityFlipped = false;
+private var clickSwitch: boolean = false;
 
 function Update() {
-	if(Input.GetKeyDown("w")) gravityFlipped = true;
-	if(Input.GetKeyDown("s")) gravityFlipped = false;
+	if(Input.GetButtonDown ("Fire1")){
+        clickSwitch = !clickSwitch;
+ 
+        if (clickSwitch){
+        	gravityFlipped = true;
+            print("Up");
+            audio.PlayOneShot(Clap);
+        } else {
+        	gravityFlipped = false;
+            print("Down");
+            audio.PlayOneShot(Clap);
+        }
+    }
+    //if(Input.GetKeyDown("r")) Application.LoadLevel("Level1");
+    if(Input.GetKeyDown("r")) transform.position = Vector3(-90,3,0);
+	//if(Input.GetKeyDown("w")) gravityFlipped = true;
+	//if(Input.GetKeyDown("s")) gravityFlipped = false;
 	
 	rigidbody.velocity.x = SPEED;
 	rigidbody.velocity.y = gravityFlipped ? GRAVITY : -GRAVITY;
---
+++ Infinirunner/Assets/Scripts/SlowMotion.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlowMotion : MonoBehaviour {
+	private CharacterController player; //Stuff like this is always private unless you are are manually setting it in the editor
+	public float SlowAmount; //With this public you can edit it in the editor
+	public float SlowRate;
+	private AudioSource[] SoundSources;
+	public string SlomoKey = "space";
+
+	// Use this for initialization
+	void Start () {
+		player = GetComponent<CharacterController>(); //Grab the CharacterController component
+		Debug.Log( player ); //Check if it worked (comment out when done...if you feel like it, whatev.)
+
+		SoundSources = FindObjectsOfType(typeof(AudioSource)) as AudioSource[]; //Check objects for an AudioSource and add  to array
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//CharacterControllers have a velocity property so let's check if we are moving
+		if( Input.GetKey( SlomoKey.ToLower() ) )
+		{
+			Time.timeScale = Mathf.Lerp(Time.timeScale, SlowAmount, SlowRate); //TimeScale affects anything using Time.deltaTime (A lot of stuff...even particles!)
+			Time.fixedDeltaTime = 0.02F * Time.timeScale; 
+			//We have to do this because physics runs on its own discrete timer, 
+			//50 fps by default, which setting timeScale to .5 effectively halves.
+			//In short, this keeps movement from being jerky.
+
+			foreach( AudioSource source in SoundSources )//Loop through stored AudioSources and change their pitch
+			{
+				source.pitch = Time.timeScale+0.3F;
+			}
+		}
+		else
+		{
+			Time.timeScale = Mathf.Lerp(Time.timeScale, 1, SlowRate);
+			//Time.timeScale = 1.0F; //If not moving then put it back to normal time scale
+			Time.fixedDeltaTime = 0.02F * Time.timeScale; //I do this just in case but probably not needed
+
+			foreach( AudioSource source in SoundSources ) //Loop through stored AudioSources and change their pitch
+			{
+				source.pitch = Time.timeScale;
+			}
+		}
+
+	}
+}
