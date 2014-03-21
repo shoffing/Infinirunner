7edef2ac912405c10712f657af2ac75b24a8af97
Steven Hoffing <shoffing@gmail.com>
2014-03-20 20:18:09 -0400

    added init stuff

---
+++ Infinirunner/Assets/PlayerControl.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+private final static var SPEED = 50;
+private final static var GRAVITY = 14;
+
+private var gravityFlipped = false;
+
+function Update() {
+	if(Input.GetKeyDown("w")) gravityFlipped = true;
+	if(Input.GetKeyDown("s")) gravityFlipped = false;
+	
+	rigidbody.velocity.x = SPEED;
+	rigidbody.velocity.y = gravityFlipped ? GRAVITY : -GRAVITY;
+}
