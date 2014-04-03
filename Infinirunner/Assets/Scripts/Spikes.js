c2dc38230e5383373b754286e9d165758e0f156d
Steven Hoffing <shoffing@gmail.com>
2014-04-03 18:43:57 -0400

    Modified spikes to make them prettier, level1 stuff

--- Infinirunner/Assets/Scripts/Spikes.js
+++ Infinirunner/Assets/Scripts/Spikes.js
@@ -1,5 +1,10 @@
 ﻿#pragma strict
 
+function Start() {
+	// Randomly rotate spikes for visual effect
+	transform.Find("SpikesRedModel").transform.localRotation.y += Random.value * 360;
+}
+
 function OnTriggerEnter(other : Collider) {
 	if(other.gameObject.tag == "Player") {
 		other.gameObject.GetComponent(PlayerControl).killPlayer();
