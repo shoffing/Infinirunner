745d675cb391edf0f8200b389152e22694d9d220
shoffing <shoffing@gmail.com>
2014-04-17 15:48:37 -0400

    Modify CameraControl to include a declaration of Z distance

--- Infinirunner/Assets/Scripts/CameraControl.js
+++ Infinirunner/Assets/Scripts/CameraControl.js
@@ -1,5 +1,6 @@
 ﻿#pragma strict
 
+private static final var CAM_DISTANCE_Z : float = 13;
 private static final var PERCENT : float = 0.25;
 
 var player : GameObject;
@@ -12,9 +13,9 @@ function Start() {
 function Update() {
 	// Calculate horizontal FOV, distance to player on Z axis, and then finally how much to offset along X
 	var hfov = 2 * Mathf.Atan(Mathf.Tan(cam.fieldOfView * Mathf.Deg2Rad / 2) * cam.aspect);
-	var camDistZ = Mathf.Abs(transform.position.z - player.transform.position.z);
-	var camOffsetX = 2 * Mathf.Tan(hfov / 2) * camDistZ * (0.5 - PERCENT);
+	var camOffsetX = 2 * Mathf.Tan(hfov / 2) * CAM_DISTANCE_Z * (0.5 - PERCENT);
 	
 	transform.position.x = player.transform.position.x + camOffsetX;
 	transform.position.y += (player.transform.position.y - transform.position.y) * 0.05;
+	transform.position.z = player.transform.position.z - CAM_DISTANCE_Z;
 }
